4b3e26b3bf89f9573a27a9042fc1492722e7cc46
srinivasankalap <cnu.ankalap@gmail.com>
2023-10-03 17:19:05 +0530

    error handling and network calls

--- src/App.js
+++ src/App.js
@@ -1,15 +1,60 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useEffect } from 'react';
+import { toggleAction } from './components/Store/Toggle';
+import Notification from './components/UI/Notification';
+
+let isInitial=true;
 
 function App() {
   const show=useSelector(state=> state.toggle.isShowing);
+  const cart=useSelector(state=>state.cart);
+  const notification=useSelector(state=>state.toggle.notification);
+  const dispatch=useDispatch();
+
+  useEffect(()=>{
+    const sendCardData=async()=>{
+      dispatch(toggleAction.showNotification({
+        status: "pending",
+        title: 'Sending...',
+        message: 'Sending cart data!'
+      }))
+    const response=await fetch('https://practice-fc64d-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',{
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    })
+    if (!response.ok){
+      throw new Error('Sending cart data failed');
+    }
+    dispatch(toggleAction.showNotification({
+      status: "success",
+      title: 'Success!',
+      message: 'Sent cart data successfully!'
+    }))
+  }
+  if (isInitial){
+    isInitial=false;
+    return;
+  }
+    sendCardData().catch(error=>{
+      dispatch(toggleAction.showNotification({
+        status: "error",
+        title: 'Error!',
+        message: 'Sending cart data failed!'
+      }))
+    });
+  },[cart, dispatch]);
+
   return (
+    <>
+    {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
     <Layout>
       {show && <Cart />}
       <Products />
     </Layout>
+    </>
   );
 }
 
--- src/components/Store/Toggle.js
+++ src/components/Store/Toggle.js
@@ -2,10 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const Toggle=createSlice({
     name: 'toggle',
-    initialState: {isShowing: false},
+    initialState: {isShowing: false , notification: null},
     reducers:{
         toggle(state){
             state.isShowing=!state.isShowing;
+        },
+        showNotification(state, action){
+            state.notification={status: action.payload.status, title: action.payload.title, message: action.payload.message, }
         }
     }
 })
